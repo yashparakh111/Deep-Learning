33616da10b33baab8a0900b0989c42df7bb76e57
Yash Parakh <yash.parakh111@gmail.com>
2018-01-14 16:46:31 -0600

    Added code for modeling an ANN

--- Learning Basics/Data_Preprocessing_Template/data_preprocessing.py
+++ Learning Basics/Data_Preprocessing_Template/data_preprocessing.py
@@ -16,12 +16,14 @@ imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-# Enconding categorical data
+# Encoding categorical data
+# Encoding independent variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
+# Enconding dependent variable
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
