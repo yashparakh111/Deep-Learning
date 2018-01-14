a1ca022caf2ff1caadd0366d426f0ad7df575a68
Yash Parakh <yash.parakh111@gmail.com>
2018-01-14 17:15:14 -0600

    Added code for making a single prediction

--- Supervised Learning/Artificial Neural Networks (ANN)/ann.py
+++ Supervised Learning/Artificial Neural Networks (ANN)/ann.py
@@ -61,6 +61,20 @@ classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
+# Predicting a single new observation
+""" Predict if the customer with the following info will leave the bank:
+    Geography: France
+    Credit Score: 600
+    Gender: Male
+    Age: 40
+    Tenure: 3
+    Balance: 60000
+    Number of Product: 2
+    Has Credit Card: Yes
+    Is Active Member: Yes
+    Estimated Salary: 50000"""
+new_prediction = classifier.predict(sc_X.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
+new_prediction = (new_prediction > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
